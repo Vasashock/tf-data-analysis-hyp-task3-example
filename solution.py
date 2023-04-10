3f8f169d52fb26eb8cff22dfa0148a8635b9a1dd
Vasashock <60927408+Vasashock@users.noreply.github.com>
2023-04-10 13:51:51 +0300

    Update 1

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
+chat_id = 89018174
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(data: pd.DataFrame) -> bool:
+    control_npv = data[data['group'] == 'control']['NPV']
+    test_npv = data[data['group'] == 'test']['NPV']
+    alpha = 0.01 # уровень значимости
+    
+    t, p = stats.ttest_ind(control_npv, test_npv, equal_var=True)
+    # использовать equal_var=False если выборки имеют разные дисперсии
+    
+    if p/2 < alpha and t < 0:
+        return True
+    else:
+        return False
